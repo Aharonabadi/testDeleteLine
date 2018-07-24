6b63502138934410aa869a9ee4d23501e5f94e8c
Aharon Abadi <34122871+abadiwhitesource@users.noreply.github.com>
2018-07-24 09:33:11 +0300

    init

---
+++ testParent.js
@@ -0,0 +1,23 @@
+var fs = require('fs');
+var mime = require('mime');
+var a = require('./internal/internal');
+
+const express = require('express')
+const app = express()
+
+ 
+
+
+function my_entry(x) {
+        g();
+	// console.log(mime.lookup('txt'));
+        var x= new a.B();
+        x.call();
+}
+
+function g(){
+	
+        console.log("hello g");
+}
+  
+my_entry();
